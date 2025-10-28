560270e88b8e0453eb3a544be81c28b66304f715
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-10-28 17:58:43 +0530

    feat: added list components

---
+++ src/Lists/Book_List.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import Details from '../components/Details';
+import '../components/styles/Details.css';
+import api from '../api';
+
+export default function Book_List() {
+  const [data, setdata] = useState([]);
+  const [count, setcount] = useState(0);
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
+  const getdata = () => {
+    api.get('api/found-item/')
+      .then((res) => res.data)
+      .then((data) => {
+        setdata(data);
+        const bookCount = data.reduce((acc, element) => {
+          return acc + (element.keywords.toLowerCase() === 'books' ? 1 : 0);
+        }, 0);
+
+        setcount(bookCount);
+      })
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <>
+      <h1 className='heading'>FOUND ITEMS</h1>
+      <div className="box">
+        {count === 0 ? (
+          <h3>NO ITEM TO DISPLAY</h3>
+        ) : (
+          data.map((element) => {
+            const name = element.keywords.toLowerCase();
+            if (name === 'books') {
+              return (
+                <div key={element.id}>
+                  <Details description={element.description} src={element.img} name={element.name} address={element.address} phone={element.phone_no} />
+                </div>
+              );
+            }
+            return null;
+          })
+        )}
+      </div>
+    </>
+  );
+}
+
---
+++ src/Lists/Digital_Devices_List.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import Details from '../components/Details';
+import '../components/styles/Details.css';
+import api from '../api';
+
+export default function Digital_Devices_List() {
+  const [data, setdata] = useState([]);
+  const [count, setcount] = useState(0);
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
+  const getdata = () => {
+    api.get('api/found-item/')
+      .then((res) => res.data)
+      .then((data) => {
+        setdata(data);
+        const counted = data.reduce((acc, element) => {
+          const name = element.keywords.toLowerCase();
+          return acc + (element.keywords.toLowerCase() === 'digital-devices' ? 1 : 0);
+        }, 0);
+
+        setcount(counted);
+      })
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <>
+      <h1 className='heading'>FOUND ITEMS</h1>
+      <div className="box">
+        {count === 0 ? (
+          <h3>NO ITEM TO DISPLAY</h3>
+        ) : (
+          data.map((element) => {
+            const name = element.keywords.toLowerCase();
+            if (name === 'digital-devices') {
+              return (
+                <div key={element.id}>
+                  <Details description={element.description} src={element.img} name={element.name} address={element.address} phone={element.phone_no} />
+                </div>
+              );
+            }
+            return null;
+          })
+        )}
+      </div>
+    </>
+  );
+}
